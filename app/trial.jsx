aa0591ab5f4cf8851960d797acb74c97b06653f6
mustafa <msolmaz83@gmail.com>
2023-08-10 23:20:08 +0200

    useRouter

--- app/trial.jsx
+++ app/trial.jsx
@@ -1,11 +1,19 @@
 'use client'
 import React from 'react'
-
+import { useRouter } from 'next/navigation'
+//butona vereceksen  unu kullanabilirsin
+//burada dikka tedilecek olan use client yap,azsan sayaayi hat ailirsin
 const Trial = () => {
-
+    const touterRed = useRouter();
     console.log('trial')
   return (
-    <div>trial</div>
+    <div>
+      
+      <div onClick={()=>touterRed.push('./about')}> 
+        push
+      </div>
+      
+      trial</div>
   )
 }
 
