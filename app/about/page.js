c2c5100c93106cc84200c769a0c5d07dc5094999
mustafa <msolmaz83@gmail.com>
2023-08-10 23:26:46 +0200

    useSeraParams

--- app/about/page.js
+++ app/about/page.js
@@ -1,6 +1,14 @@
+'use client'
+
 import React from 'react'
+import { useSearchParams } from 'next/navigation'
 
 const Page = () => {
+
+const searchParam = useSearchParams()
+const name = searchParam.get('name')
+console.log(name,'name')
+
   return (
     <div>P
         aboooooooooooooooo
